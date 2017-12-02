54375fbf92efa77bbdbb8b72bb790c9c3d820673
sa2taka <sa2taka@gmail.com>
2017-12-02 13:28:52 +0900

    変更: memberに関する表示を変更

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -28,7 +28,7 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @member.save
-        format.html { redirect_to @member, notice: 'Member was successfully created.' }
+        format.html { redirect_to @member, notice: 'メンバーが新しく作られました' }
         format.json { render :show, status: :created, location: @member }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class MembersController < ApplicationController
   def update
     respond_to do |format|
       if @member.update(member_params)
-        format.html { redirect_to @member, notice: 'Member was successfully updated.' }
+        format.html { redirect_to @member, notice: 'メンバーが更新されました' }
         format.json { render :show, status: :ok, location: @member }
       else
         format.html { render :edit }
