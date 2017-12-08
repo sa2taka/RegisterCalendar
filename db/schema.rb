f0341a06dec69e5fcc4b15f38ce9db89a18ee7d0
sa2taka <sa2taka@gmail.com>
2017-12-08 23:37:30 +0900

    変更: イベントモデルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20171130061409) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.datetime "start"
-    t.datetime "end"
+    t.datetime "event_date"
+    t.time "start"
+    t.time "end"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
