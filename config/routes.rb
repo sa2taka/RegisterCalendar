ce12eebb5ff164a8c48c4ec0528681724dbe15a8
sa2taka <sa2taka@gmail.com>
2017-12-11 20:56:58 +0900

    追加: ログインとルートとrootルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'events#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :events
   resources :members
