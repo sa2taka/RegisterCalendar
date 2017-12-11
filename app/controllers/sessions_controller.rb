218eeb95075bc295a6839fecd3f6ae2d3b2e7501
sa2taka <sa2taka@gmail.com>
2017-12-11 21:28:16 +0900

    追加:ログイン認証と失敗時の動作を定義

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
+  require 'openssl'
+
+  PASS_DIGEST = "774fcded1e8ae0bf66bc0bbb9dab74dc1da6387c414b3d64a0cfeebb0d346737"
+  PASS_DIGEST.freeze
+
   def new
   end
+
+  def create
+    pass = params[:session][:passcode]
+    digest = OpenSSL::Digest.new('sha256')
+    digest.update(pass)
+
+    if params[:session][:id] == "sat" && digest.to_s == PASS_DIGEST
+    else
+      flash.now[:danger] = "idかパスコードが間違っています"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
