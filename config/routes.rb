596cc1e9c831053a6c55bc5ad2bd8975ff1bb35d
sa2taka <sa2taka@gmail.com>
2017-12-11 20:50:10 +0900

    追加: Sessionsコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :events
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
