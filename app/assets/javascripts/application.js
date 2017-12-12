c8f00d38c3bcdb09db4f7728849b706a63bbdd34
sa2taka <sa2taka@gmail.com>
2017-12-12 22:25:06 +0900

    追加: リストに検索機能を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require dataTables/jquery.dataTables
 //= require jquery
+//= require dataTables/jquery.dataTables
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
